2069cb1857a671630470092f5ac5bf499d3d3a39
artveronica <44465813+artveronica@users.noreply.github.com>
2019-02-19 20:31:17 +0100

    Add files via upload

--- examples/example_07/main.js
+++ examples/example_07/main.js
@@ -1,3 +1,7 @@
+//The MIT License (MIT)
+
+//Copyright (c) 2016 Tal Ater ??
+
 //This Javascript code was taken from https://www.jumpingrivers.com/blog/voice-control-your-shiny-apps/
 //Credits to Jumping Rivers
 
@@ -9,6 +13,7 @@ if (annyang) {
     "Jumping Rivers": function() {
       //opens a new tab with this website
       window.open("https://www.jumpingrivers.com", "_blank");
+      console.log("this works"); //Just for checking
     }
   };
   // register the defined commands
