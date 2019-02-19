bcb5ce25cf2d4d175a93f080d14a985d792ae6ab
Willowsounds <35813856+Willowsounds@users.noreply.github.com>
2019-02-19 13:45:17 +0100

    Create main.js

---
+++ examples/example_07/main.js
@@ -0,0 +1,18 @@
+//This Javascript code was taken from https://www.jumpingrivers.com/blog/voice-control-your-shiny-apps/
+//Credits to Jumping Rivers
+
+// detect whether it is supported in the browser
+if (annyang) {
+  // define some commands
+  var commands = {
+    // on hearing "Jumping Rivers", call this function
+    "Jumping Rivers": function() {
+      //opens a new tab with this website
+      window.open("https://www.jumpingrivers.com", "_blank");
+    }
+  };
+  // register the defined commands
+  annyang.addCommands(commands);
+  // start listening
+  annyang.start();
+}
