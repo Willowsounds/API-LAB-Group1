f4c1597038df584f35115496721522103b038fc1
artveronica <44465813+artveronica@users.noreply.github.com>
2019-02-19 20:25:22 +0100

    Add files via upload

---
+++ examples/example_02/main.js
@@ -0,0 +1,38 @@
+//The MIT License (MIT)
+
+//Copyright (c) 2016 Tal Ater ??
+
+//defining functions for PLAYING AND PAUSING A VIDEO on voice command via DOM
+
+var video = document.getElementById("vid");
+
+//function for PLAYING A VIDEO
+function Play() {
+  video.play();
+}
+
+//function for PAUSING A VIDEO
+function Pause() {
+  video.pause();
+}
+
+if (annyang) {
+  var commands = {
+    "play video": function() {
+      vid.play();
+      console.log("this works");
+    },
+    "pause video": function() {
+      vid.pause();
+      console.log("this pauses");
+    }
+  };
+
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.start({
+    autoRestart: true
+  });
+}
